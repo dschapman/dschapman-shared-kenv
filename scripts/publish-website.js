8c23e6726cf686c972dfa13eaf4fac70147add1d
Daniel Chapman <scriptor.deo@gmail.com>
2022-03-23 08:07:23 -0400

    pushed from Script Kit

--- scripts/publish-website.js
+++ scripts/publish-website.js
@@ -9,48 +9,45 @@ let website_notes = "/Users/danielchapman/Library/Mobile Documents/iCloud~md~obs
 let website_dir = "/Users/danielchapman/github/dschapman/dschapman-com"
 
 let today = Date()
-let output
+let output = []
 let input = await arg("Publish:", [{ name: "Website Content", description: "Publish any changes to poems, articles or blog posts", "value": "Website Content" }, { name: "Website Notes", description: "Publish any changes to public notes", value: "Website Notes"  }, {name: "All Content", description:"Publish it all", value: "All Content"}])
 // -C allows git to be run from any directory
 switch (input) {
     case "Website Content":
-        output = await exec(`git -C "${website_content}" add -A`)
-        console.log(output.stdout)
-        output = await exec(`git -C "${website_content}" commit -m "Publish content: ${today.toString()}"`)
-        console.log(output.stdout)
-        output = await exec(`git -C "${website_content}" push`)
-        console.log(output.stdout)
-        output = await exec(`git -C "${website_dir}" submodule update --remote`)
-        console.log(output.stdout)
-        output = await exec(`git -C "${website_dir}" add dschapman-com-content`)
-        console.log(output.stdout)
-        output = await exec(`git -C "${website_dir}" commit -m "Publish content: ${today.toString()}"`)
-        console.log(output.stdout)
-        output = await exec(`git -C "${website_dir}" push`)
-        console.log(output.stdout)
+        output.push(await exec(`git -C "${website_content}" add -A`))
+        output.push(await exec(`git -C "${website_content}" commit -m "Publish content: ${today.toString()}"`))
+        output.push(await exec(`git -C "${website_content}" push`))
+        output.push(await exec(`git -C "${website_dir}" submodule update --remote`))
+        output.push(await exec(`git -C "${website_dir}" add dschapman-com-content`))
+       output.push(await exec(`git -C "${website_dir}" commit -m "Publish content: ${today.toString()}"`))
+        output.push(await exec(`git -C "${website_dir}" push`))
         break;
     case "Website Notes":
-        await exec(`git -C "${website_notes}" add -A`)
-        await exec(`git -C "${website_notes}" commit -m "Publish content: ${today.toString()}"`)
-        await exec(`git -C "${website_notes}" push`)
-        await exec(`git -C "${website_dir}" submodule update --remote`)
-        await exec(`git -C "${website_dir}" add My-notes`)
-        await exec(`git -C "${website_dir}" commit -m "Publish notes: ${today.toString()}"`)
-        await exec(`git -C "${website_dir}" push`)
+        output.push(await exec(`git -C "${website_notes}" add -A`))
+        output.push(await exec(`git -C "${website_notes}" commit -m "Publish content: ${today.toString()}"`))
+        output.push(await exec(`git -C "${website_notes}" push`))
+        output.push(await exec(`git -C "${website_dir}" submodule update --remote`))
+        output.push(await exec(`git -C "${website_dir}" add My-notes`))
+        output.push(await exec(`git -C "${website_dir}" commit -m "Publish notes: ${today.toString()}"`))
+        output.push(await exec(`git -C "${website_dir}" push`))
         break;
     case "All":
-        await exec(`git -C "${website_content}" add -A`)
-        await exec(`git -C "${website_content}" commit -m "Publish content: ${today.toString()}"`)
-        await exec(`git -C "${website_content}" push`)
-        await exec(`git -C "${website_notes}" add -A`)
-        await exec(`git -C "${website_notes}" commit -m "Publish content: ${today.toString()}"`)
-        await exec(`git -C "${website_notes}" push`)
-        await exec(`git -C "${website_dir}" submodule update --remote`)
-        await exec(`git -C "${website_dir}" add My-notes dschapman-com-content`)
-        await exec(`git -C "${website_dir}" commit -m "Publish all content: ${today.toString()}"`)
-        await exec(`git -C "${website_dir}" push`)
+        output.push(await exec(`git -C "${website_content}" add -A`))
+        output.push(await exec(`git -C "${website_content}" commit -m "Publish content: ${today.toString()}"`))
+        output.push(await exec(`git -C "${website_content}" push`))
+        output.push(await exec(`git -C "${website_notes}" add -A`))
+        output.push(await exec(`git -C "${website_notes}" commit -m "Publish content: ${today.toString()}"`))
+        output.push(await exec(`git -C "${website_notes}" push`))
+        output.push(await exec(`git -C "${website_dir}" submodule update --remote`))
+        output.push(await exec(`git -C "${website_dir}" add My-notes dschapman-com-content`))
+        output.push(await exec(`git -C "${website_dir}" commit -m "Publish all content: ${today.toString()}"`))
+        output.push(await exec(`git -C "${website_dir}" push`))
         break;
     default:
         break;
 }
+
+output.forEach(item => {
+    console.log(item.stdout)
+})
 
